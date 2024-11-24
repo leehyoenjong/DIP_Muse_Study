7802026de61501c1268f2f65bda8aa5cdbb08b71
leehyoenjong <eenddl7541@gmail.com>
2024-11-24 16:11:18 +0900

    마무리

--- Assets/02_Script/Bullet.cs
+++ Assets/02_Script/Bullet.cs
@@ -1,14 +1,22 @@
-// 2024-11-23 AI-Tag
+// 2024-11-24 AI-Tag
 // This was created with assistance from Muse, a Unity Artificial Intelligence product
 
 using System;
 using UnityEditor;
 using UnityEngine;
+
 public class Bullet : MonoBehaviour
-{ 
+{
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.tag == "Monster")
+        {
+            Destroy(other.gameObject);
+        }
+
         // 총알은 충돌 후 파괴됩니다.
         Destroy(gameObject);
+
+
     }
 }
--- Assets/02_Script/NPC/State.cs
+++ Assets/02_Script/NPC/State.cs
@@ -6,5 +6,6 @@ public enum State
 {
     Move,
 	Attack,
-	Wait
+	Wait,
+	Die
 }
